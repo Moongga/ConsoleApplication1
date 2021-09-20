578e8df31d6718d80c99eddec08308505b3b4ba0
Alyona <armin@74-13>
2021-09-20 16:05:42 +0300

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,10 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hellow!\n";
+}
